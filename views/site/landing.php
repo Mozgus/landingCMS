4a9ae8b225da3f0c7f4ed45641392c9f19d61360
Mozgus <retailnet@rambler.ru>
2018-11-06 17:49:42 +0300

    Initial commit

---
+++ views/site/landing.php
@@ -0,0 +1,20 @@
+<section id="header">
+    <div class="jumbotron" id="header_logo">
+        <img src="/../images/logo.png" alt="logo">
+        <h1>Сайт на Yii2 за 5 дней</h1>
+        <h2>Хотите получить красивый сайт за 5 дней?<br>
+            Тогда добро пожаловать к нам!</h2>
+    </div>
+</section>
+<section id="audience">
+
+</section>
+<section id="about">
+
+</section>
+<section id="reviews">
+
+</section>
+<section id="prices">
+
+</section>
