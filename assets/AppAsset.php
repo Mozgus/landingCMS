bad17f117c56ee03e6b526c5d03e3e187dcc6e67
Mozgus <retailnet@rambler.ru>
2018-11-07 22:31:36 +0300

    Logo animation

--- assets/AppAsset.php
+++ assets/AppAsset.php
@@ -28,4 +28,9 @@ class AppAsset extends AssetBundle
         'yii\web\YiiAsset',
         'yii\bootstrap\BootstrapAsset',
     ];
+
+    public $jsOptions = [
+        'position' => \yii\web\View::POS_HEAD
+    ];
+
 }
--- views/site/landing.php
+++ views/site/landing.php
@@ -1,6 +1,14 @@
 <section id="header">
     <div class="jumbotron" id="header_logo">
-        <img src="/../images/logo.png" alt="logo">
+        <img src="/../images/logo.png" alt="logo"
+             id="logo_img" style="display: none">
+        <script>
+            $(document).ready(
+                function () {
+                    $("#logo_img").slideDown("slow");
+                }
+            );
+        </script>
         <h1>Сайт на Yii2 за 5 дней</h1>
         <h2>Хотите получить красивый сайт за 5 дней?<br>
             Тогда добро пожаловать к нам!</h2>
